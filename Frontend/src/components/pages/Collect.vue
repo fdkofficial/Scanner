6f898017c667172dbba997e8688f99d36020965a
Tausif <zakariatausif2@gmail.com>
2023-05-06 14:36:07 +0530

    Add Collect and Drop page

--- Frontend/src/components/pages/Collect.vue
+++ Frontend/src/components/pages/Collect.vue
@@ -1,16 +1,127 @@
 <template>
-    <div class="m-2">
-        <div id="reader" width="600px"></div>
+    <div class="container-fluid">
+        <h4 class="p-0 px-3 m-0 my-4 lh-lg">Department</h4>
+        <div class="accordion" id="accordionExample">
+            <!-- For loop come here -->
+            <div class="accordion-item my-5" v-for="(i, index) in list_departments" :key="index">
+                <h2 class="accordion-header custom-bg-3 rounded-2">
+                    <button
+                        :class="index == 0 ? 'accordion-button shadow-none p-2 custom-bg-3 text-white rounded-2' : 'accordion-button shadow-none p-2 custom-bg-3 text-white rounded-2 collabsed'"
+                        type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse' + index" aria-expanded="true"
+                        aria-controls="collapse{id}">
+                        {{ i.name }}
+                    </button>
+                </h2>
+                <div :id="'collapse' + index"
+                    :class="index == 0 ? 'accordion-collapse collapse show' : 'accordion-collapse collapse'"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body p-2">
+                        <div class="d-flex flex-row flex-wrap justify-content-evenly gap-2 p-0 m-0">
+                            <div class="my-2" v-for="j in i.units" :key="j">
+                                <input @change="addUnits(j.id)" type="checkbox" class="btn-check" :id="'btn-check-' + j.id"
+                                    autocomplete="off" data-bs-toggle="modal" data-bs-target="#UnitsModal">
+                                <label class="btn custom-bg-2 pt-2 text-white btn-sm" :for="'btn-check-' + j.id">{{ j.name
+                                }}</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="UnitsModal" tabindex="-1" aria-labelledby="UnitsModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header custom-bg-3">
+                    <h1 class="modal-title fs-5 text-white" id="UnitsModalLabel">Scan Sample</h1>
+                    <button type="button" class="btn-close" style="filter:invert(1)" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="reader" style="width:100%; height:50vh;"></div>
+                    <div class="table-responsive my-3">
+                        <table class="table table-bordered table-stripped text-center">
+                            <col style="width:25%;">
+                            <col style="width:70%;">
+                            <col style="width:5%;">
+                            <thead>
+                                <tr class="custom-bg-3 text-white">
+                                    <th>Unit</th>
+                                    <th>Sample No</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Unit</td>
+                                    <td>Sample No</td>
+                                    <td><button class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn custom-bg-2 text-white">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import { Html5QrcodeScanner } from "html5-qrcode";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import Sample from "../../services/Sample"
 export default {
     setup() {
+        let sampleData = ref({
+            "sample_no": [],
+            "origin": [],
+        })
+
+        const addUnits = (val) => {
+            if (sampleData.value.origin.filter((fil) => fil == val).length > 0) {
+                sampleData.value.origin = sampleData.value.origin.filter((fil) => fil != val)
+                // alert("unselected")
+            }
+            else {
+                sampleData.value.origin.push(val);
+                // alert("new cal selected")
+            }
+        }
+
+        let list_departments = ref();
+        let list_lab = ref();
         const onScanSuccess = (decodedText, decodedResult) => {
             // handle the scanned code as you like, for example:
             console.log(`Code matched = ${decodedText}`, decodedResult);
+            let qr = document.getElementById('CodeScan')
+            qr.innerHTML = decodedText
+            sampleData.value.sample_no.push(decodedText)
+        }
+
+        const listDepartment = () => {
+            let data = new Sample();
+            data.Department().then((response => {
+                list_departments.value = response.data.data;
+            }))
+        }
+
+        const addSampleData = () => {
+            let data = new Sample();
+            data.PostSampleData().then((response => {
+                list_departments.value = response.data.data;
+            }))
+        }
+
+
+        const listLaberatory = () => {
+            let data = new Sample();
+            data.Laberatory().then((response => {
+                list_lab.value = response.data.data;
+            }))
         }
 
         const onScanFailure = (error) => {
@@ -18,16 +129,32 @@ export default {
             // for example:
             console.warn(`Code scan error = ${error}`);
         }
+
         onMounted(() => {
+            listDepartment();
+            listLaberatory();
             let html5QrcodeScanner = new Html5QrcodeScanner(
                 "reader",
                 { fps: 10, qrbox: { width: 400, height: 179 } },
       /* verbose= */ false);
             html5QrcodeScanner.render(onScanSuccess, onScanFailure);
         })
+
+        const dNone = () => {
+            const btn = document.getElementById('html5-qrcode-button-camera-permission')
+            btn.click()
+            console.log('clicked')
+        }
+
         return {
             onScanSuccess,
-            onScanFailure
+            onScanFailure,
+            list_departments,
+            list_lab,
+            addUnits,
+            sampleData,
+            listLaberatory,
+            dNone
         }
     }
 }
--- Frontend/src/components/pages/Drop.vue
+++ Frontend/src/components/pages/Drop.vue
@@ -1,16 +1,113 @@
 <template>
-    <div class="m-2">
-        <div id="reader" width="600px"></div>
+    <div class="container-fluid">
+        <h4 class="p-0 m-0 my-4 lh-lg">Destination</h4>
+        <div class="my-4" v-for="lab in list_lab" :key="lab">
+            <input type="checkbox" class="btn-check " id="destination{id}" autocomplete="off" data-bs-toggle="modal"
+                data-bs-target="#UnitsModal">
+            <label class="btn custom-bg-3 text-white shadow-sm pt-2 btn-sm w-100 rounded-4" for="destination{id}">{{
+                lab.name
+            }}</label>
+        </div>
+
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="UnitsModal" tabindex="-1" aria-labelledby="UnitsModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content">
+                <div class="modal-header custom-bg-3">
+                    <h1 class="modal-title fs-5 text-white" id="UnitsModalLabel">Scan Sample</h1>
+                    <button type="button" class="btn-close" style="filter:invert(1)" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="reader" style="width:100%; height:50vh;"></div>
+                    <div class="mb-3">
+                        <label for="ReciverId" class="form-label">Reciver ID</label>
+                        <input type="text" class="form-control" id="ReciverId" placeholder="Reciver Id">
+                    </div>
+                    <div class="mb-3">
+                        <table class="table table-bordered table-stripped text-center">
+                            <col style="width:25%;">
+                            <col style="width:70%;">
+                            <col style="width:5%;">
+                            <thead>
+                                <tr class="custom-bg-3 text-white">
+                                    <th>Unit</th>
+                                    <th>Sample No</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Unit</td>
+                                    <td>Sample No</td>
+                                    <td><button class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn custom-bg-2 text-white">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import { Html5QrcodeScanner } from "html5-qrcode";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import Sample from "../../services/Sample"
 export default {
     setup() {
+        let sampleData = ref({
+            "sample_no": [],
+            "origin": [],
+        })
+
+        const addUnits = (val) => {
+            if (sampleData.value.origin.filter((fil) => fil == val).length > 0) {
+                sampleData.value.origin = sampleData.value.origin.filter((fil) => fil != val)
+                // alert("unselected")
+            }
+            else {
+                sampleData.value.origin.push(val);
+                // alert("new cal selected")
+            }
+        }
+
+        let list_departments = ref();
+        let list_lab = ref();
         const onScanSuccess = (decodedText, decodedResult) => {
             // handle the scanned code as you like, for example:
             console.log(`Code matched = ${decodedText}`, decodedResult);
+            let qr = document.getElementById('CodeScan')
+            qr.innerHTML = decodedText
+            sampleData.value.sample_no.push(decodedText)
+        }
+
+        const listDepartment = () => {
+            let data = new Sample();
+            data.Department().then((response => {
+                list_departments.value = response.data.data;
+            }))
+        }
+
+        const addSampleData = () => {
+            let data = new Sample();
+            data.PostSampleData().then((response => {
+                list_departments.value = response.data.data;
+            }))
+        }
+
+
+        const listLaberatory = () => {
+            let data = new Sample();
+            data.Laberatory().then((response => {
+                list_lab.value = response.data.data;
+            }))
         }
 
         const onScanFailure = (error) => {
@@ -18,16 +115,32 @@ export default {
             // for example:
             console.warn(`Code scan error = ${error}`);
         }
+
         onMounted(() => {
+            listDepartment();
+            listLaberatory();
             let html5QrcodeScanner = new Html5QrcodeScanner(
                 "reader",
                 { fps: 10, qrbox: { width: 400, height: 179 } },
       /* verbose= */ false);
             html5QrcodeScanner.render(onScanSuccess, onScanFailure);
         })
+
+        const dNone = () => {
+            const btn = document.getElementById('html5-qrcode-button-camera-permission')
+            btn.click()
+            console.log('clicked')
+        }
+
         return {
             onScanSuccess,
-            onScanFailure
+            onScanFailure,
+            list_departments,
+            list_lab,
+            addUnits,
+            sampleData,
+            listLaberatory,
+            dNone
         }
     }
 }
